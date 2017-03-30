5a3b8498d657c1c3108fece2877e824365b53e1e
Sumeet Ranka <sumeet.ranka47@gmail.com>
2017-03-30 18:05:38 +0530

    Minor UI and some functional changes

--- alumniportal/static/assets/js/main-body.js
+++ alumniportal/static/assets/js/main-body.js
@@ -1,19 +1,16 @@
 function openModal(image){
-        var modal = document.getElementById('myModal');
+  var modal = document.getElementById('myModal');
 
-        // Get the image and insert it inside the modal - use its "alt" text as a caption
-        var img = document.getElementById('myImg');
-        var modalImg = document.getElementById("img01");
-        var captionText = document.getElementById("caption");
-            modal.style.display = "block";
-            modalImg.src = image.src;
-            captionText.innerHTML = image.alt;
-        var span = document.getElementsByClassName("close")[0];
-        span.onclick = function() {
-            modal.style.display='none';
-        }
-
-
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var modalImg = document.getElementById("img01");
+  // var captionText = document.getElementById("caption");
+  modal.style.display = "block";
+  modalImg.src = image.src;
+  // captionText.innerHTML = image.alt;
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = function() {
+      modal.style.display='none';
+  }
 }
 
 $(window).load(function(){
